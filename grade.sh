7811d637d2dad241a94a372072b0b270f3a7ba9f
Rashid Abder Rahim <rabder24@gmail.com>
2023-05-11 16:28:21 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -21,6 +21,8 @@ fi
 
 cp student-submission/ListExamples.java TestListExamples.java grading-area
 
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/*java
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
