6452118887e5812478328bd2d9d21238a3d02fa0
Rashid Abder Rahim <rabder24@gmail.com>
2023-05-25 16:49:10 -0700

    Grader now displays the names of the tests you failed

--- grade.sh
+++ grade.sh
@@ -28,20 +28,40 @@ if [[ $? -eq 0 ]]
     echo "Files compiled succesfully"
 else
     echo "Your files failed to compile"
+    exit 1
 fi
 
 cd grading-area
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > junit.txt
 
+# Detect if it any tests failed by looking at the JUnit output
 grep "Failures:" junit.txt > result.txt
 if [[ $? -eq 0 ]]
     then
     echo "Failures found"
     # Find the failure count
-    cat result.txt
+    grep -oE "[0-9]+" result.txt > failure_count.txt
+    testsRan=$(grep -m 1 '' failure_count.txt)
+    fails=$(grep -m 2 '' failure_count.txt | sed -n '2p')
+    echo "Out of" $testsRan "tests, you failed" $fails ":"
+
+    # Convert to integer
+    testsRan_Int=$((testsRan))
+    fails_int=$((fails))
+
+    # Pick names of methods that failed 
+    grep ") " junit.txt > tests_failed.txt
+
+    cat "tests_failed.txt"
+
+    # Calculate the score
+    score=$((100 - $fails_int * 100 / $testsRan_Int))
+    echo "Your score is" $score "out of 100"
+
 
 else
-    echo "No failure"
+    echo "No errors. All tests passed"
+    echo 100
 
 fi
 
